4cf4e4a3b9fe535f0da6ba03e2af46f0eae7078d
abengier <andrew.bengier@gmail.com>
2023-03-31 17:30:41 -0400

    Update ui for independent development

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -7,10 +7,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('app') as HTMLElement
 );
 root.render(
-  // TODO: reenable strict mode pre-production release
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
